da273851704dbbacb00173b6c5513ea55a872528
Laufer4u <nzevgolis4u@gmail.com>
2021-01-25 22:22:55 +0200

    2. Python Structures

--- main.py
+++ main.py
@@ -1,42 +1,58 @@
-name = 'Francis'
-slice = name[0:3] # name[a, b] = name[a] + ... + name[b-1]
-print(slice)
+employees = [
+    {"name": "John Mckee", "age":38, "department":"Sales"},
+    {"name": "Lisa Crawford", "age":29, "department":"Marketing"},
+    {"name": "Sujan Patel", "age":33, "department":"HR"}
+]
+print(employees)
+for employee in employees:
+    print("Name:", employee['name'])
+    print("Age:", employee['age'])
+    print("Department:", employee['department'])
+    print('-' * 20)
 
-def sliceName(s = str, L = list):
-    test = ''
-    n = len(list(s))
-    for i in range(L[0], L[1]):
-        test += s[i]
-        print(test)
-print(sliceName(s = 'nikolakis', L = [2, 8]))
+items = ['apple', 'orange', 'banana']
+quantity = [5,3,2]
+orders = zip(items,quantity)
+print(orders)
+orders = zip(items,quantity)
+print(list(orders))
+orders = zip(items,quantity)
+print(tuple(orders))
+orders = zip(items,quantity)
+print(dict(orders))
+
+###############################
+
+orders = {'apple':5, 'orange':3, 'banana':2}
+print(orders.values())
+print(list(orders.values()))
+print(list(orders.keys()))
+for tuple in list(orders.items()):
+  print(tuple)
 
 
-#################
-#
-#not A
-#Y and Z
-#Y or Z
-#
-#################
+###############################
 
-name1 = 'Likos'
-name2 = 'Nikos'
-print(name1<name2)
-'''
-1. Vital Python: Math, Strings, Conditionals, and Loops
-
-Summary
-You have gone over a lot of material in this introductory chapter. 
-You have covered math operations, string concatenation and methods, 
-general Python types, variables, conditionals, and loops. Combining 
-these elements allows us to write programs of real value.
-Additionally, we have been learning Python syntax. You now understand 
-some of the most common errors, and you're becoming accustomed to the 
-importance that indentation plays. You're learning how to leverage 
-important keywords such as range, in, if, and True and False.
-Going forward, you now have the key fundamental skills required of 
-all Python programmers. Although there is much to learn, you have
- a vital foundation in place to build upon the types and techniques discussed here.
-Coming up next, you will learn about some of the most important Python types, 
-including lists, dictionaries, tuples, and sets.
-'''
+s5 = {1,2,3,4}
+s6 = {3,4,5,6}
+print(s5 | s6)
+print(s5.union(s6))
+print(s5 & s6)
+print(s5.intersection(s6))
+print(s5 - s6)
+print(s5.difference(s6))
+print(s5 <= s6)
+print(s5.issubset(s6))
+s7 = {1,2,3}
+s8 = {1,2,3,4,5}
+print(s7 <= s8)
+print(s7.issubset(s8))
+print(s7 < s8)
+s9 = {1,2,3}
+s10 = {1,2,3}
+print(s9 < s10)
+print(s9 < s9)
+print(s8 >= s7)
+print(s8.issuperset(s7))
+print(s8 > s7)
+print(s8 > s8)
